0254bf99510e4756247f1bcdcc609a160218f7f5
myname21-meet <myname21@meet.mit.edu>
2019-07-29 07:12:14 -0400

    yuval is not correct

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,20 +8,36 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(a_topic, a_rating):
+	article_object=Knowledge(a_topic=a_topic,a_rating=a_rating)
+	session.add(article_object)
+	session.commit()
+add_article("chicken", 10)
+add_article("firends", 11)
+add_article("himym",3)	
+
 
 def query_all_articles():
-	pass
+	artic=session.query(Knowledge).all()
+	return artic 
+print(query_all_articles())
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(a_topic):
+	top = session.query(Knowledge).filter_by(a_topic=a_topic).first()
+	return top
+print(query_article_by_topic("chicken"))
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(a_topic):
+	d_top=session.query(Knowledge).filter_by(a_topic=a_topic).delete()
+	session.commit()
+	
+print(delete_article_by_topic("firends"))
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+	session.commit()
+delete_all_articles()
 
 def edit_article_rating():
-	pass
+	alert= session.query(Knowledge).all()
+	
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -13,5 +13,25 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__='Knowledge'
+	a_id = Column(Integer, primary_key=True)
+	a_topic= Column(String)
+	a_rating=Column(Integer)
 
-	pass
+	def __repr__(self):
+		return ("Knowledge id: {}\n"
+			"Knowledge topic: {}\n"
+			"Knowledge rating: {}\n").format(
+			self.a_id,
+			self.a_topic,
+			self.a_rating
+			)
+	def bad_good(a_rating):
+		if a_rating<7:
+			print("Unfortunately, this article does not have a better rating. Maybe, this is an article that should bereplaced soon")
+		else:
+			print("a great article")
+article1= Knowledge(a_topic="friends", a_rating=11)
+article2 = Knowledge(a_topic="himym",a_rating=3)
+article3=Knowledge(a_topic="languageus",a_rating=8)
+
